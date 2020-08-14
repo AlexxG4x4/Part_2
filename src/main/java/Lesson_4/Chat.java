3f95e7e295adad55cf3c9b543ff7dc9cf1ee2e53
Aleksandr <alexgurin86@gmail.com>
2020-08-14 09:58:37 +0300

    Задание к блоку 2 урок 4.

---
+++ src/main/java/Lesson_4/Chat.java
@@ -0,0 +1,63 @@
+package Lesson_4;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Chat extends JFrame {
+
+        private int width = 900;
+        private int sizeX = 850;
+        private int sizeY = 350;
+
+        public void initField() {
+            setTitle("Чат");
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            setBounds(150,150, width, sizeY);
+
+            setLayout(new BorderLayout());
+
+            JPanel controlPanel = new JPanel();
+            JPanel desk = new JPanel();
+
+            JTextArea dialogField = new JTextArea();
+            dialogField.setEditable(false);
+            dialogField.setLineWrap(true);
+            dialogField.setWrapStyleWord(true);
+            dialogField.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+            JScrollPane jsp = new JScrollPane(dialogField,
+                    JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                    JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            JTextField textField = new JTextField();
+            textField.addActionListener(action -> {
+                sendMessage(textField, dialogField);
+            });
+
+            JButton sendMessage = new JButton("Отправить");
+            sendMessage.addActionListener(action -> {
+                sendMessage(textField, dialogField);
+            });
+
+            controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.X_AXIS));
+            controlPanel.add(textField);
+            controlPanel.add(sendMessage);
+            controlPanel.setSize(width - sizeX, sizeY);
+
+            desk.setLayout(new BoxLayout(desk, BoxLayout.X_AXIS));
+            desk.add(jsp);
+            desk.setSize(sizeX, sizeY);
+
+            getContentPane().add(controlPanel, BorderLayout.SOUTH);
+            getContentPane().add(desk, BorderLayout.CENTER);
+
+            setVisible(true);
+        }
+    private void sendMessage(JTextField textField, JTextArea dialogField) {
+        if (textField.getText().equals("")) {
+            return;
+        }
+
+        dialogField.append("\n" + textField.getText());
+        textField.setText("");
+    }
+}
